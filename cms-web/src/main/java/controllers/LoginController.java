ee01b4ec13cda42be060dcdc28815b54c60bc2dd
Talal Alrawajfeh <TalalAlrawajfeh@gmail.com>
2017-02-12 20:42:24 +0200

    edited urls in jsps for bootsrap and css files

---
+++ cms-web/src/main/java/controllers/LoginController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+	public ModelAndView login(String name, String password) {
+		return null;
+	}
+}
