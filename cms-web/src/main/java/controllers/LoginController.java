f60924cd70b131b7a8bf40b35f0491fdc14ea5f4
Talal Alrawajfeh <TalalAlrawajfeh@gmail.com>
2017-02-13 14:13:30 +0200

    Now LoginController works

--- cms-web/src/main/java/controllers/LoginController.java
+++ cms-web/src/main/java/controllers/LoginController.java
@@ -1,26 +1,50 @@
 package controllers;
 
+import java.util.Objects;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import beans.User;
+import entities.UserEntity;
+import persistence.UserRepository;
+
+@Configuration("loginController")
 @Controller
 public class LoginController {
+	@Autowired
+	private UserRepository userRepository;
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public ModelAndView login() {
-
 		return new ModelAndView("Login");
 	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public ModelAndView login(HttpServletRequest req, HttpServletResponse resp, @RequestParam String username,
 			@RequestParam String password) {
+		// ApplicationContext context = new
+		// ClassPathXmlApplicationContext("spring.xml");
+		// userRepository = context.getBean(UserRepository.class);
+
+		UserEntity userEntity = userRepository.findByUsername(username);
+		if (Objects.isNull(userEntity)) {
+			System.out.println("null");
+		}
+
+		User user = new User();
+		user.setUsername(username);
+		user.setHashedPassword(password);
+
+		req.getSession().setAttribute("user", user);
 		return new ModelAndView("Login");
 	}
 }
--- cms-web/src/main/java/persistence/UserRepository.java
+++ cms-web/src/main/java/persistence/UserRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import entities.UserEntity;
 
 @Repository
-public interface UserPersistence extends CrudRepository<UserEntity, Serializable> {
-
+public interface UserRepository extends CrudRepository<UserEntity, Serializable> {
+	UserEntity findByUsername(String username);
 }
