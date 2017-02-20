a6bf9c0dd83c4dd2c91fb2334e9679fbd602e0bc
Talal Alrawajfeh <TalalAlrawajfeh@gmail.com>
2017-02-20 14:25:14 +0200

    Fixed SonarLint issues in class InvalidUserException

--- cms-web/src/main/java/usecases/InvalidUserException.java
+++ cms-web/src/main/java/usecases/InvalidUserException.java
@@ -1,41 +1,46 @@
 package usecases;
 
 public class InvalidUserException extends Exception {
-	private static final long serialVersionUID = 8106985756540152268L;
+    private static final long serialVersionUID = 8106985756540152268L;
 
-	private InvalidUserCause invalidUserCause;
+    private final InvalidUserCause invalidUserCause;
 
-	public InvalidUserException(InvalidUserCause invalidUserCause) {
-		super();
-		this.invalidUserCause = invalidUserCause;
-	}
+    public InvalidUserException(InvalidUserCause invalidUserCause) {
+        super();
+        this.invalidUserCause = invalidUserCause;
+    }
 
-	public InvalidUserException() {
-		super();
-	}
+    public InvalidUserException() {
+        super();
+        invalidUserCause = InvalidUserCause.OTHER;
+    }
 
-	public InvalidUserException(String message, Throwable cause, boolean enableSuppression,
-			boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
+    public InvalidUserException(String message, Throwable cause, boolean enableSuppression,
+                                boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        invalidUserCause = InvalidUserCause.OTHER;
+    }
 
-	public InvalidUserException(String message, Throwable cause) {
-		super(message, cause);
-	}
+    public InvalidUserException(String message, Throwable cause) {
+        super(message, cause);
+        invalidUserCause = InvalidUserCause.OTHER;
+    }
 
-	public InvalidUserException(String message) {
-		super(message);
-	}
+    public InvalidUserException(String message) {
+        super(message);
+        invalidUserCause = InvalidUserCause.OTHER;
+    }
 
-	public InvalidUserException(Throwable cause) {
-		super(cause);
-	}
+    public InvalidUserException(Throwable cause) {
+        super(cause);
+        invalidUserCause = InvalidUserCause.OTHER;
+    }
 
-	public InvalidUserCause getInvalidUserCause() {
-		return invalidUserCause;
-	}
+    public InvalidUserCause getInvalidUserCause() {
+        return invalidUserCause;
+    }
 
-	public enum InvalidUserCause {
-		USER_NOT_FOUND, INVALID_PASSWORD;
-	}
+    public enum InvalidUserCause {
+        USER_NOT_FOUND, INVALID_PASSWORD, OTHER;
+    }
 }
