22be532dc2f3facd7ac5ebe2b4a5bc70c170c92b
Talal Alrawajfeh <TalalAlrawajfeh@gmail.com>
2017-02-20 14:16:02 +0200

    Fixed SonarLint issues in class User

--- cms-web/src/main/java/beans/User.java
+++ cms-web/src/main/java/beans/User.java
@@ -1,84 +1,88 @@
 package beans;
 
-import java.io.Serializable;
-
 import utils.HashUtil;
+
+import java.io.Serializable;
 
 /**
  * @author u624
- *
  */
 public class User implements Serializable {
-	private static final long serialVersionUID = 2561036855901068794L;
+    private static final long serialVersionUID = 2561036855901068794L;
 
-	private String username;
-	private String passwordHashCode;
-	private String fullName;
-	private Boolean enabled;
+    private String username;
+    private String passwordHashCode;
+    private String fullName;
+    private Boolean enabled;
 
-	public User() {
-		/* default constructor */
-	}
+    public User() {
+        /* default constructor */
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public String getPasswordHashCode() {
-		return passwordHashCode;
-	}
+    public String getPasswordHashCode() {
+        return passwordHashCode;
+    }
 
-	public void setPasswordHashCode(String passwordHashCode) {
-		this.passwordHashCode = passwordHashCode;
-	}
+    public void setPasswordHashCode(String passwordHashCode) {
+        this.passwordHashCode = passwordHashCode;
+    }
 
-	public void setHashedPassword(String password) {
-		passwordHashCode = HashUtil.hashString(password);
-	}
+    public void setHashedPassword(String password) {
+        passwordHashCode = HashUtil.hashString(password);
+    }
 
-	public String getFullName() {
-		return fullName;
-	}
+    public String getFullName() {
+        return fullName;
+    }
 
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
-	}
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 
-	public Boolean getEnabled() {
-		return enabled;
-	}
+    public Boolean getEnabled() {
+        return enabled;
+    }
 
-	public Boolean isEnabled() {
-		return enabled;
-	}
+    public Boolean isEnabled() {
+        return enabled;
+    }
 
-	public void setEnabled(Boolean enabled) {
-		this.enabled = enabled;
-	}
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
 
-	@Override
-	public String toString() {
-		return "User [username=" + username + ", passwordHashCode=" + passwordHashCode + ", fullName=" + fullName
-				+ ", enabled=" + enabled + "]";
-	}
+    @Override
+    public String toString() {
+        return "User [username=" + username + ", passwordHashCode=" + passwordHashCode + ", fullName=" + fullName
+                + ", enabled=" + enabled + "]";
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (obj instanceof User) {
-			User user = (User) obj;
+    @Override
+    public int hashCode() {
+        return fullName.hashCode() ^ username.hashCode() ^ passwordHashCode.hashCode() ^ enabled.hashCode();
+    }
 
-			if (enabled == user.isEnabled() && username.equals(user.getUsername())
-					&& fullName.equals(user.getFullName()) && passwordHashCode.equals(user.getPasswordHashCode())) {
-				return true;
-			}
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof User) {
+            User user = (User) obj;
 
-			return false;
-		}
-		
-		return super.equals(obj);
-	}
+            if (enabled.equals(user.isEnabled()) && username.equals(user.getUsername())
+                    && fullName.equals(user.getFullName()) && passwordHashCode.equals(user.getPasswordHashCode())) {
+                return true;
+            }
+
+            return false;
+        }
+
+        return super.equals(obj);
+    }
 }
