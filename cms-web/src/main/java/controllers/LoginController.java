5a3a8a3908c69471103297f9ef01ef4e3d0ec7b4
Talal Alrawajfeh <TalalAlrawajfeh@gmail.com>
2017-02-20 14:21:14 +0200

    Fixed SonarLint issues in class LoginController Class

--- cms-web/src/main/java/controllers/LoginController.java
+++ cms-web/src/main/java/controllers/LoginController.java
@@ -1,13 +1,6 @@
 package controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.function.Consumer;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import beans.User;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,79 +8,84 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import beans.User;
 import usecases.InvalidUserException;
 import usecases.InvalidUserException.InvalidUserCause;
 import usecases.LoginUseCase;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.function.Consumer;
+
 @Controller
 public class LoginController {
-	private static final String INCORRECT_USERNAME_OR_PASSWORD_MESSAGE = "Incorrect username or password";
-	private static final String ATTEMPT_TO_LOGIN_WITH_UNKNOWN_USER_LOG = "An attempt to login with unknown user ";
-	private static final String FAILED_TO_LOGIN_INCORRECT_PASSWORD_LOG = " has failed to log in due to incorrect password";
-	private static final String USER_RETURNED_TO_LOGIN_PAGE_LOG = " has returned to login page and will be redirected to user management page";
-	private static final String USER_SESSION_ATTRIBUTE_NAME = "user";
-	private static final String USER_HAS_LOGGED_IN_LOG = " has logged in";
-	private static final String DISABLED_USER_MESSAGE = "The user is disabled";
-	private static final String USER_MANAGEMENT_PAGE = "redirect:/user-management";
-	private static final String LOGIN_CONTROLLER_URL = "/login";
-	private static final String ERROR_MESSAGE = "errorMessage";
-	private static final String USER_STRING = "User ";
-	private static final String SHOW_ERROR = "showError";
-	private static final String LOGIN_PAGE = "Login";
+    private static final String INCORRECT_USERNAME_OR_PASSWORD_MESSAGE = "Incorrect username or password";
+    private static final String ATTEMPT_TO_LOGIN_WITH_UNKNOWN_USER_LOG = "An attempt to login with unknown user ";
+    private static final String FAILED_TO_LOGIN_INCORRECT_PASSWORD_LOG = " has failed to log in due to incorrect password";
+    private static final String USER_RETURNED_TO_LOGIN_PAGE_LOG = " has returned to login page and will be redirected to user management page";
+    private static final String USER_SESSION_ATTRIBUTE_NAME = "user";
+    private static final String USER_HAS_LOGGED_IN_LOG = " has logged in";
+    private static final String DISABLED_USER_MESSAGE = "The user is disabled";
+    private static final String USER_MANAGEMENT_PAGE = "redirect:/user-management";
+    private static final String LOGIN_CONTROLLER_URL = "/login";
+    private static final String ERROR_MESSAGE = "errorMessage";
+    private static final String USER_STRING = "User ";
+    private static final String SHOW_ERROR = "showError";
+    private static final String LOGIN_PAGE = "Login";
 
-	private static Logger logger = Logger.getLogger(LoginController.class);
-	private Map<InvalidUserCause, Consumer<String>> causeMap = new HashMap<>();
+    private static Logger logger = Logger.getLogger(LoginController.class);
+    private EnumMap<InvalidUserCause, Consumer<String>> causeMap = new EnumMap<>(new HashMap<>());
 
-	{
-		causeMap.put(InvalidUserCause.INVALID_PASSWORD,
-				(username) -> logger.warn(USER_STRING + username + FAILED_TO_LOGIN_INCORRECT_PASSWORD_LOG));
+    @Autowired
+    private LoginUseCase loginUseCase;
 
-		causeMap.put(InvalidUserCause.USER_NOT_FOUND,
-				(username) -> logger.info(ATTEMPT_TO_LOGIN_WITH_UNKNOWN_USER_LOG + username));
-	}
+    public LoginController() {
+        causeMap.put(InvalidUserCause.INVALID_PASSWORD,
+                username -> logger.warn(USER_STRING + username + FAILED_TO_LOGIN_INCORRECT_PASSWORD_LOG));
 
-	@Autowired
-	private LoginUseCase loginUseCase;
+        causeMap.put(InvalidUserCause.USER_NOT_FOUND,
+                username -> logger.info(ATTEMPT_TO_LOGIN_WITH_UNKNOWN_USER_LOG + username));
+    }
 
-	@RequestMapping(value = LOGIN_CONTROLLER_URL, method = RequestMethod.GET)
-	public ModelAndView login(HttpServletRequest req, HttpServletResponse resp) {
-		req.setAttribute(SHOW_ERROR, false);
-		User user = (User) req.getSession().getAttribute(USER_SESSION_ATTRIBUTE_NAME);
+    @RequestMapping(value = LOGIN_CONTROLLER_URL, method = RequestMethod.GET)
+    public ModelAndView login(HttpServletRequest req, HttpServletResponse resp) {
+        req.setAttribute(SHOW_ERROR, false);
+        User user = (User) req.getSession().getAttribute(USER_SESSION_ATTRIBUTE_NAME);
 
-		if (Objects.nonNull(user)) {
-			logger.info(USER_STRING + user.getUsername() + USER_RETURNED_TO_LOGIN_PAGE_LOG);
-			
-			return new ModelAndView(USER_MANAGEMENT_PAGE);
-		}
+        if (Objects.nonNull(user)) {
+            logger.info(USER_STRING + user.getUsername() + USER_RETURNED_TO_LOGIN_PAGE_LOG);
 
-		return new ModelAndView(LOGIN_PAGE);
-	}
+            return new ModelAndView(USER_MANAGEMENT_PAGE);
+        }
 
-	@RequestMapping(value = LOGIN_CONTROLLER_URL, method = RequestMethod.POST)
-	public ModelAndView login(HttpServletRequest req, HttpServletResponse resp, @RequestParam String username,
-			@RequestParam String password) {
+        return new ModelAndView(LOGIN_PAGE);
+    }
 
-		try {
-			User user = loginUseCase.validateUser(username, password);
+    @RequestMapping(value = LOGIN_CONTROLLER_URL, method = RequestMethod.POST)
+    public ModelAndView login(HttpServletRequest req, HttpServletResponse resp, @RequestParam String username,
+                              @RequestParam String password) {
 
-			if (user.isEnabled()) {
-				logger.info(USER_STRING + username + USER_HAS_LOGGED_IN_LOG);
-				req.getSession(true).setAttribute(USER_SESSION_ATTRIBUTE_NAME, user);
-				
-				return new ModelAndView(USER_MANAGEMENT_PAGE);
-			}
+        try {
+            User user = loginUseCase.validateUser(username, password);
 
-			req.setAttribute(ERROR_MESSAGE, DISABLED_USER_MESSAGE);
-		} catch (InvalidUserException e) {
-			logger.error(e);
-			causeMap.get(e.getInvalidUserCause()).accept(username);
-			req.setAttribute(ERROR_MESSAGE, INCORRECT_USERNAME_OR_PASSWORD_MESSAGE);
-		}
+            if (user.isEnabled()) {
+                logger.info(USER_STRING + username + USER_HAS_LOGGED_IN_LOG);
+                req.getSession(true).setAttribute(USER_SESSION_ATTRIBUTE_NAME, user);
 
-		req.setAttribute(SHOW_ERROR, true);
-		
-		return new ModelAndView(LOGIN_PAGE);
-	}
+                return new ModelAndView(USER_MANAGEMENT_PAGE);
+            }
+
+            req.setAttribute(ERROR_MESSAGE, DISABLED_USER_MESSAGE);
+        } catch (InvalidUserException e) {
+            logger.error(e);
+            causeMap.get(e.getInvalidUserCause()).accept(username);
+            req.setAttribute(ERROR_MESSAGE, INCORRECT_USERNAME_OR_PASSWORD_MESSAGE);
+        }
+
+        req.setAttribute(SHOW_ERROR, true);
+
+        return new ModelAndView(LOGIN_PAGE);
+    }
 }
