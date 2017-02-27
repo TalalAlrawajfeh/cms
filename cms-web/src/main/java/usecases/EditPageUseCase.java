e49c0af882621da75d16fdaa4846307a771aaefc
Talal Alrawajfeh <TalalAlrawajfeh@gmail.com>
2017-02-27 16:45:14 +0200

    Removed unnecessary methods in EditPageUseCase

--- cms-web/src/main/java/usecases/EditPageUseCase.java
+++ cms-web/src/main/java/usecases/EditPageUseCase.java
@@ -2,20 +2,13 @@ package usecases;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import beans.Page;
 import persistence.PageRepository;
 
 public class EditPageUseCase {
-	private static final String URI_SEPERATOR = "/";
-
 	@Autowired
 	private PageRepository pageRepository;
 
 	public void deletePageByUri(String uri) {
 		pageRepository.delete(pageRepository.findByUri(uri));
-	}
-
-	public String getPagePureUri(Page page) {
-		return page.getUri().substring(page.getUri().lastIndexOf(URI_SEPERATOR));
 	}
 }
