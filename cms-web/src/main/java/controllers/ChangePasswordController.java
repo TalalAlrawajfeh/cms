5dc7630611f1cf011900eaed3995f56211ce2b88
Talal Alrawajfeh <TalalAlrawajfeh@gmail.com>
2017-02-27 09:41:15 +0200

    Fixed sonar lint issues in ChangePasswordController

--- cms-web/src/main/java/controllers/ChangePasswordController.java
+++ cms-web/src/main/java/controllers/ChangePasswordController.java
@@ -17,29 +17,29 @@ import utils.HashUtil;
 
 @Controller
 public class ChangePasswordController {
-	private static final String INCORRECT_PASSWORD_MESSAGE = "The old password is incorrect";
 	private static final String REDIRECT_USER_MANAGEMENT = "redirect:/user-management";
 	private static final String INCLUDED_PAGE_ATTRIBUTE = "includedPage";
 	private static final String ERROR_MESSAGE_ATTRIBUTE = "errorMessage";
+	private static final String INCORRECT_PASS_MESSAGE = "The old password is incorrect";
 	private static final String USER_SESSION_ATTRIBUTE = "user";
 	private static final String CURRENT_USER_ATTRIBUTE = "currentUser";
 	private static final String SHOW_ERROR_ATTRIBUTE = "showError";
-	private static final String CHANGE_PASSWORD_JSP = "ChangePassword";
-	private static final String CHANGE_PASSWORD_URL = "/change-password";
+	private static final String CHANGE_PASS_JSP = "ChangePassword";
+	private static final String CHANGE_PASS_URL = "/change-password";
 	private static final String CANCEL_ACTION = "cancel";
 	private static final String BASE_JSP = "Base";
 
 	@Autowired
 	private EditUserUseCase editUserUseCase;
 
-	@RequestMapping(value = CHANGE_PASSWORD_URL, method = RequestMethod.GET)
+	@RequestMapping(value = CHANGE_PASS_URL, method = RequestMethod.GET)
 	public ModelAndView changePassword(HttpServletRequest req, HttpServletResponse resp) {
 		setProperAttribtutes(req, false);
 
 		return new ModelAndView(BASE_JSP);
 	}
 
-	@RequestMapping(value = CHANGE_PASSWORD_URL, method = RequestMethod.POST)
+	@RequestMapping(value = CHANGE_PASS_URL, method = RequestMethod.POST)
 	public ModelAndView changePassword(HttpServletRequest req, HttpServletResponse resp, @RequestParam String action,
 			@RequestParam String oldPassword, @RequestParam String newPassword) {
 
@@ -66,10 +66,10 @@ public class ChangePasswordController {
 	private void setProperAttribtutes(HttpServletRequest req, boolean showErrorMessage) {
 		req.setAttribute(CURRENT_USER_ATTRIBUTE, (User) req.getSession().getAttribute(USER_SESSION_ATTRIBUTE));
 		req.setAttribute(SHOW_ERROR_ATTRIBUTE, showErrorMessage);
-		req.setAttribute(INCLUDED_PAGE_ATTRIBUTE, CHANGE_PASSWORD_JSP);
+		req.setAttribute(INCLUDED_PAGE_ATTRIBUTE, CHANGE_PASS_JSP);
 
 		if (showErrorMessage) {
-			req.setAttribute(ERROR_MESSAGE_ATTRIBUTE, INCORRECT_PASSWORD_MESSAGE);
+			req.setAttribute(ERROR_MESSAGE_ATTRIBUTE, INCORRECT_PASS_MESSAGE);
 		}
 	}
 }
