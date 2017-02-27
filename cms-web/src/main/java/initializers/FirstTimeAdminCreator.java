bdcffb50391b199877b4fdf2dbaf67fc8cceaa5e
Talal Alrawajfeh <TalalAlrawajfeh@gmail.com>
2017-02-27 09:47:56 +0200

    Fixed sonar lint issues in FirstTimeAdminCreator

--- cms-web/src/main/java/initializers/FirstTimeAdminCreator.java
+++ cms-web/src/main/java/initializers/FirstTimeAdminCreator.java
@@ -8,15 +8,15 @@ import persistence.UserRepository;
 import utils.CopyUtil;
 
 public class FirstTimeAdminCreator extends AbstractInitializer<UserRepository> {
-	private static final String COMPLEX_PASSWORD = "P@ssw0rd";
 	private static final String ADMIN_USERNAME = "admin";
+	private static final String COMPLEX = "P@ssw0rd";
 
 	@Override
 	public void initialize() {
 		if (Objects.isNull(repository.findByUsername(ADMIN_USERNAME))) {
 			repository.save(CopyUtil.createAndCopyFields(UserEntity.class,
 					new UserBuilder().setEnabled(true).setFullName(ADMIN_USERNAME).setUsername(ADMIN_USERNAME)
-							.setHashedPassword(COMPLEX_PASSWORD).build()));
+							.setHashedPassword(COMPLEX).build()));
 		}
 	}
 }
