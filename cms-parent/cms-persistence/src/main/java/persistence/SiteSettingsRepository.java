e421636b9c235430536f7234649da9e3da5382e4
Talal Alrawajfeh <TalalAlrawajfeh@gmail.com>
2017-02-28 11:29:06 +0200

    added site settings use case

--- cms-parent/cms-persistence/src/main/java/persistence/SiteSettingsRepository.java
+++ cms-parent/cms-persistence/src/main/java/persistence/SiteSettingsRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.repository.CrudRepository;
 import entities.SiteSettingsEntity;
 
 public interface SiteSettingsRepository extends CrudRepository<SiteSettingsEntity, Serializable> {
-
+	SiteSettingsEntity findByDeliveryUrl(String deliveryUrl);
 }
