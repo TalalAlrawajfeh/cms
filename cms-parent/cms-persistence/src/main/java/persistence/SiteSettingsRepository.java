36bc04d18ee6d4ed12fd1b0a6497feffc02c1aa0
Talal Alrawajfeh <TalalAlrawajfeh@gmail.com>
2017-02-28 11:01:50 +0200

    Added site settings repository

---
+++ cms-parent/cms-persistence/src/main/java/persistence/SiteSettingsRepository.java
@@ -0,0 +1,11 @@
+package persistence;
+
+import java.io.Serializable;
+
+import org.springframework.data.repository.CrudRepository;
+
+import entities.SiteSettingsEntity;
+
+public interface SiteSettingsRepository extends CrudRepository<SiteSettingsEntity, Serializable> {
+
+}
